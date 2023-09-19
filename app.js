a342465d979862e8c6d68948681faba0e5804c0d
Bongani Baloyi <bonganibaloyi94@gmail.com>
2023-09-19 18:37:23 +0200

    update

--- app.js
+++ app.js
@@ -8,7 +8,6 @@ const session = require('express-session');
 const methodOverride = require('method-override');
 
 const connectDB = require('./server/config/db')
-const isActiveRoute = require('./server/helpers/routeHelpers');
 
 const app = express();
 const PORT = 3000 || process.env.PORT;
@@ -39,8 +38,6 @@ app.use(express.static('public'));
 app.use(expressLayout);
 app.set('layout', './layouts/main');
 app.set('view engine', 'ejs');
-
-app.locals.isActiveRoute = isActiveRoute;
 
 // From server
 app.use('/', require('./server/router/main'));
